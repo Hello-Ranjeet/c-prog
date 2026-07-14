73fc924071353b5473e3fad2c5c380c19a5a5d76
Ranjeet Prajapati <60763740+Hello-Ranjeet@users.noreply.github.com>
2026-07-15 01:59:34 +0530

    Correct input/output functions in addition2.c
    Fix syntax errors and correct print function usage.

---
+++ addition2.c
@@ -0,0 +1,12 @@
+#include<conio.h>
+#include<stdio.h>
+void main()
+{
+  int a,b;
+  print("Enter 1st no: ");
+  scanf("%d",&a);
+  printf("\nEnter 2nd no: ");
+  scanf("%d",&b);
+  print("\nSum = %d", a+b);
+  getch();
+}
